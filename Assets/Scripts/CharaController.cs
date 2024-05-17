1e2cc1324b27fc06773659b4655d66585871395a
ppiazzoni <147702180+ppiazzoni@users.noreply.github.com>
2024-05-17 16:04:06 +0200

    ok

--- Assets/Scripts/CharaController.cs
+++ Assets/Scripts/CharaController.cs
@@ -1,23 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharaController : MonoBehaviour
 {
     public float movespeed;
     public Rigidbody2D rb2d;
     private Vector2 moveInput;
-
-    private float ActiveMoveSpeed
+    public Canvas deathCanvas;
+    public GameObject Player;
+    private float ActiveMoveSpeed;
     public float DashSpeed;
+
+    public float DashLength = .5f, DashCooldown = 1f;
+
+    public float DashCounter;
+    public float DashCoolCounter;
+
     [Header("VFX")]
     [SerializeField] private ParticleSystem m_deathVFX;
     private bool m_isAlive = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ActiveMoveSpeed = movespeed;
     }
+
 
     // Update is called once per frame
     void Update()
@@ -27,8 +36,36 @@ public class CharaController : MonoBehaviour
         
         moveInput.Normalize();
         
-        rb2d.velocity = moveInput * movespeed;
+        rb2d.velocity = moveInput * ActiveMoveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (DashCoolCounter <= 0 && DashCounter <= 0)
+            {
+                ActiveMoveSpeed = DashSpeed;
+                DashCounter = DashLength;
+             
+            }
+        }
+
+        if (DashCounter > 0)
+        {
+            DashCounter -= Time.deltaTime;
+
+            if(DashCounter <= 0)
+            {
+                ActiveMoveSpeed = movespeed;
+                DashCoolCounter = DashCooldown;
+                
+            }
+        }
+
+        if(DashCoolCounter > 0)
+        {
+            DashCoolCounter -= Time.deltaTime;
+        }
     }
+
 
     public void TakeDamage(DiscController disc)
     {
@@ -40,7 +77,10 @@ public class CharaController : MonoBehaviour
         Debug.Log("Player_Killed");
         m_deathVFX.Play();
         m_isAlive = false;
+        deathCanvas.gameObject.SetActive(true);
+        Player.gameObject.SetActive(false);
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Disc"))
@@ -49,6 +89,7 @@ public class CharaController : MonoBehaviour
             if(disc)
             {
                 TakeDamage(disc);
+                
             }
         }
         else
